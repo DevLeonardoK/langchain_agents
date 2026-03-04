dd888333fcecfa7816d23d292eae698ebcf6e80d
Leonardo Kremer <devleonardok@gmail.com>
2026-03-03 22:55:35 -0300

    Start developing a weather agent – my first commit

---
+++ 1_agent_quick_start_langchain/main.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from langchain_deepseek import ChatDeepSeek
+from langchain.agents import create_agent
+from langchain.tools import tool
+from requests import get
+import os
+
+load_dotenv()
+weather_cordinates_api_key =  os.getenv("WEATHER_CORDINATES_API_KEY")
+
+#Tools
+
+@tool("get_latitude_longitude", description="Get the latitude and longitude of a location")
+def get_cordinates(city_name: str, country_name: str) -> str:
+    """"Tool responsible to get the latitude and longitude of a location"""
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_name}&limit=5&appid={weather_cordinates_api_key}"
+    response = get(url)
+    
+
+
+@tool("find_weather", description="Find the weather using the user's input location")
+def get_weather(location: str) -> str:
+    """"Tool responsible to find the weather using the user's input location"""
+
+
+if __name__ == "__main__":
+    print("Hello from langchain-agents!")
