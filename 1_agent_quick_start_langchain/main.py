c2946464ac32e33cb28e130fad6d9f391dce8fe2
Leonardo Kremer <devleonardok@gmail.com>
2026-03-04 20:52:54 -0300

    complete get_cordinates function

--- 1_agent_quick_start_langchain/main.py
+++ 1_agent_quick_start_langchain/main.py
@@ -11,10 +11,18 @@ weather_cordinates_api_key =  os.getenv("WEATHER_CORDINATES_API_KEY")
 #Tools
 
 @tool("get_latitude_longitude", description="Get the latitude and longitude of a location")
-def get_cordinates(city_name: str, country_name: str) -> str:
+def get_cordinates(city_name: str, country_code: str) -> str:
     """"Tool responsible to get the latitude and longitude of a location"""
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_name}&limit=5&appid={weather_cordinates_api_key}"
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&limit=5&appid={weather_cordinates_api_key}"
     response = get(url)
+    if response.status_code in (200,201):
+        data = response.json()
+        if data:
+            latitude = data[0]['lat']
+            longitude = data[0]['lon']
+            return f"Latitude: {latitude}, Longitude: {longitude}"
+        else:
+            return "No data found"
     
 
 
@@ -22,6 +30,7 @@ def get_cordinates(city_name: str, country_name: str) -> str:
 def get_weather(location: str) -> str:
     """"Tool responsible to find the weather using the user's input location"""
 
-
 if __name__ == "__main__":
     print("Hello from langchain-agents!")
+
+    
