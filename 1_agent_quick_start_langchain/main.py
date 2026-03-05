c37a39b33816ce48b47e82fb668cdc64c45ce228
Leonardo Kremer <devleonardok@gmail.com>
2026-03-04 21:28:08 -0300

    complete get_weather function

--- 1_agent_quick_start_langchain/main.py
+++ 1_agent_quick_start_langchain/main.py
@@ -12,7 +12,7 @@ weather_cordinates_api_key =  os.getenv("WEATHER_CORDINATES_API_KEY")
 
 @tool("get_latitude_longitude", description="Get the latitude and longitude of a location")
 def get_cordinates(city_name: str, country_code: str) -> str:
-    """"Tool responsible to get the latitude and longitude of a location"""
+    """"Tool responsible to get the latitude and longitude of a location, the first step to find the weather is to get the cordinates of the location, this tool will be used in the agent's workflow"""
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&limit=5&appid={weather_cordinates_api_key}"
     response = get(url)
     if response.status_code in (200,201):
@@ -26,10 +26,21 @@ def get_cordinates(city_name: str, country_code: str) -> str:
     
 
 
-@tool("find_weather", description="Find the weather using the user's input location")
-def get_weather(location: str) -> str:
-    """"Tool responsible to find the weather using the user's input location"""
-
+@tool("find_weather", description="Find the weather using the user's input location (latitude and longitude)")
+def get_weather(latitude: str, longitude: str) -> str:
+    """"Tool responsible to find the weather using the user's input location (latitude and longitude). This tool will be used to return the temperature, humidity, general weather and wind speed of the location, """
+    link = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={weather_cordinates_api_key}&lang=en&units=metric"
+    response = get(link)
+    if response.status_code in (200,201):
+        data = response.json()
+        if data:
+            general_weather = data['weather'][0].description
+            temperature = data['main']['temp']
+            humidity = data['main']['humidity']
+            wind_speed = data['wind']['speed']
+            return f"General Weather: {general_weather},\n Temperature: {temperature}°C,\n Humidity: {humidity}%,\n Wind Speed: {wind_speed} m/s"
+            
+            
 if __name__ == "__main__":
     print("Hello from langchain-agents!")
 
