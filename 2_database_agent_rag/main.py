279462a13d61e925de01465bd5631e0f89138e34
Leonardo Kremer <devleonardok@gmail.com>
2026-03-05 21:42:32 -0300

    feat: add tool to verify neo4j connection

--- 2_database_agent_rag/main.py
+++ 2_database_agent_rag/main.py
@@ -15,3 +15,19 @@ USERNAME = os.getenv("USERNAME_NEO4J")
 PASSWORD = os.getenv("PASSWORD_NEO4J")
 DATABASE = os.getenv("DATABASE_NEO4J")
 
+#Neo4j Database connection details
+from neo4j import GraphDatabase
+
+AUTH = (USERNAME, PASSWORD)
+
+
+@tool("verify_neo4j_connection", description="Tool to verify connection to neo4j database")
+def verify_neo4j_connection():
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        try:
+            driver.verify_connectivity()
+            return "Connected to Neo4j successfully!"
+        except Exception as e:
+            return f"Connection failed: {str(e)}"
+
+
