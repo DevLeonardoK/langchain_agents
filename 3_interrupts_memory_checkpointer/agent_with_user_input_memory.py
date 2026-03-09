cb9ff867c98f96711db0eb60b446e9266c2fbd02
Leonardo Kremer <devleonardok@gmail.com>
2026-03-09 10:01:07 -0300

    tool to listdir

--- 3_interrupts_memory_checkpointer/agent_with_user_input_memory.py
+++ 3_interrupts_memory_checkpointer/agent_with_user_input_memory.py
@@ -4,6 +4,10 @@ from typing_extensions import TypedDict, Annotated #Metadata#
 from dotenv import load_dotenv
 from langgraph.checkpoint.memory import InMemorySaver #Memory RAM
 from langchain_deepseek import ChatDeepSeek
+from langchain.tools import tool
+import os
+
+
 
 #load envs
 load_dotenv()
@@ -13,15 +17,22 @@ load_dotenv()
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-    
+
+
+#system file reader tool
+@tool("os_file_reader_tool", description="Tool for retrieving files from this directory")
+def os_file_reader_tool():
+    files = os.listdir("/home/devleonardo-ai/Documents/langchain_agents")
+    return files
+
 #LLM
-model = ChatDeepSeek(model='deepseek-chat', temperature=0)
+model = ChatDeepSeek(model='deepseek-chat', temperature=0, tool=os_file_reader_tool)
 
 #Chatbot node
-
 def chatbot(state: State):
     response = model.invoke(state['messages'])
     return {'messages': [response]}
+
 
 
 #builder = pipeline langgraph
